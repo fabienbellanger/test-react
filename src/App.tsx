f2c7ac6eb48d5ffcdf31ff52e964367e675824de
Fabien BELLANGER <valentil@gmail.com>
2025-04-15 23:10:04 +0200

    :'(

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
+// import Input from './components/Input';
+
+import Input from './components/Input';
+
 export default function App() {
     return (
         <>
-            <div>
-                TODO List
+            <div className="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden mt-16">
+                <div className="px-4 py-2">
+                    <h1 className="text-gray-800 font-bold text-2xl uppercase">
+                        To-Do List
+                    </h1>
+                </div>
+                <form className="w-full max-w-sm mx-auto px-4 py-2">
+                    <Input value="" placeholder="Add a new task" />
+                </form>
+                <ul className="divide-y divide-gray-200 px-4">
+                    <li className="py-4">
+                        <div className="flex items-center">
+                            <input
+                                id="todo1"
+                                name="todo1"
+                                type="checkbox"
+                                className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded"
+                            />
+                            <label
+                                htmlFor="todo1"
+                                className="ml-3 block text-gray-900"
+                            >
+                                <span className="text-lg font-medium">
+                                    Finish project proposal
+                                </span>
+                            </label>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </>
-    )
+    );
 }
---
+++ src/components/Input.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+interface InputProps {
+    value: string;
+    placeholder?: string;
+    // onClick: () => void;
+}
+
+export default function Input({ value, placeholder }: InputProps) {
+    const [inputValue, setInputValue] = useState(value);
+
+    return (
+        <div className="flex items-center border-b-2 border-teal-500 py-2">
+            <input
+                className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                type="text"
+                placeholder={placeholder}
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button
+                className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+                type="button"
+                onClick={(e) => console.log(e)}
+            >
+                Add
+            </button>
+        </div>
+    );
+}
