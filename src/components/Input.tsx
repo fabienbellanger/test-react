76255f811144b70bf0d1e4afd7f8384df0cf304a
Fabien Bellanger <fabien.bellanger@apitic.com>
2025-04-16 12:17:18 +0200

    Style

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -19,7 +19,7 @@ export default function Input({ value, placeholder }: InputProps) {
                 onChange={(e) => setInputValue(e.target.value)}
             />
             <button
-                className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+                className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded cursor-pointer"
                 type="button"
                 onClick={(e) => console.log(e)}
             >
