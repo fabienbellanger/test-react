e0c0b68f97b6bbe026f1efda1ae2e4b2a1b10032
Fabien Bellanger <fabien.bellanger@apitic.com>
2025-04-22 11:30:22 +0200

    Install MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Button } from '@mui/material';
 import TodoList from './components/todo_list/TodoList';
+import { Home as HomeIcon } from '@mui/icons-material';
 
 export default function App() {
-    return <TodoList />;
+    return (
+        <>
+            <div className='flex flex-row justify-center mt-8'>
+                <Button variant="contained" color="secondary">
+                    <span className='pr-2'><HomeIcon/></span>
+                    Add Todo
+                </Button>
+            </div>
+
+            <TodoList />
+
+        </>
+    );
 }
