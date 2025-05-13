a1e17c926b394956a3dc3884dbf916ae8be22edd
Fabien BELLANGER <valentil@gmail.com>
2025-05-14 00:31:22 +0200

    Improve `ChatbotMessage`

---
+++ src/components/chatbot/ChatbotMessage.tsx
@@ -0,0 +1,67 @@
+interface ChatbotMessageProps {
+    direction: 'start' | 'end';
+    message: string;
+    name?: string;
+    avatarUrl?: string;
+    time?: string;
+    footer?: string;
+}
+
+export default function ChatbotMessage({
+    direction,
+    message,
+    name = '', // Valeur par défaut
+    avatarUrl,
+    time,
+    footer,
+}: ChatbotMessageProps) {
+    const chatClasse = `chat ${
+        direction === 'start' ? 'chat-start' : 'chat-end'
+    }`;
+
+    // Fonction utilitaire pour générer l'avatar placeholder
+    const getAvatarPlaceholder = () => {
+        if (name.trim().length > 0) {
+            return name.charAt(0).toUpperCase();
+        }
+        return direction === 'start' ? 'S' : 'E';
+    };
+
+    const avatarPlaceholderClass = `w-10 rounded-full ${
+        direction === 'start'
+            ? 'bg-secondary text-secondary-content'
+            : 'bg-warning text-warning-content'
+    }`;
+
+    // Fonction utilitaire pour afficher l'avatar
+    const renderAvatar = () => {
+        if (avatarUrl) {
+            return (
+                <div className="chat-image avatar">
+                    <div className="w-10 rounded-full">
+                        <img alt="Avatar" src={avatarUrl} />
+                    </div>
+                </div>
+            );
+        }
+        return (
+            <div className="chat-image avatar avatar-placeholder">
+                <div className={avatarPlaceholderClass}>
+                    <span>{getAvatarPlaceholder()}</span>
+                </div>
+            </div>
+        );
+    };
+
+    return (
+        <div className={chatClasse}>
+            {renderAvatar()}
+            <div className="chat-header">
+                {name}
+                {time && <time className="text-xs opacity-50">{time}</time>}
+            </div>
+            <div className="chat-bubble">{message}</div>
+            {footer && <div className="chat-footer opacity-50">{footer}</div>}
+        </div>
+    );
+}
--- src/pages/Chatbot.tsx
+++ src/pages/Chatbot.tsx
@@ -1,26 +1,26 @@
 // interface ChatbotProps {}
 
-import ChatbotBubble from '../components/chatbot/ChatbotBubble';
+import ChatbotMessage from '../components/chatbot/ChatbotMessage';
 
 export default function Chatbot() {
     return (
         <div className="p-8 text-gray-200">
-            <ChatbotBubble
+            <ChatbotMessage
                 direction="start"
-                name="Obi-Wan Kenobi"
                 message="You were the Chosen One!"
+                name="Obi-Wan Kenobi"
                 time="12:45"
                 avatarUrl="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
                 footer="delivered"
             />
 
-            <ChatbotBubble
+            <ChatbotMessage
                 direction="end"
-                name="Anakin"
                 message="I hate you!"
-                time="12:46"
-                avatarUrl="https://img.daisyui.com/images/profile/demo/anakeen@192.webp"
-                footer="Seen at 12:46"
+                name="Anakin Skywalker"
+                // time="12:46"
+                // avatarUrl="https://img.daisyui.com/images/profile/demo/anakeen@192.webp"
+                // footer="Seen at 12:46"
             />
         </div>
     );
