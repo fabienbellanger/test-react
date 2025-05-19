f48d2b962adb05a448475686ea8fa8f482f93951
Fabien Bellanger <fabien.bellanger@apitic.com>
2025-05-19 18:18:42 +0200

    Change theme

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -36,7 +36,7 @@ export default function NavBar({ title }: NavBarProps) {
 
     return (
         <header>
-            <nav className="navbar bg-base-300 shadow-smflex items-center justify-between">
+            <nav className="navbar bg-base-200 shadow-sm flex items-center justify-between">
                 <div className="flex items-center gap-2">
                     <img src="/img/vite.svg" alt="Logo" className="h-6 w-6" />
                     <h1 className="text-2xl text-ellipsis whitespace-nowrap overflow-hidden">
